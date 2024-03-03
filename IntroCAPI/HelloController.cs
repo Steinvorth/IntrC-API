fdd710f22d24a53c5b22b266c8740013ea345562
Emiliano Steinvorth <75647452+Steinvorth@users.noreply.github.com>
2024-03-03 00:26:59 -0600

    Curl Command Comment

--- IntroCAPI/HelloController.cs
+++ IntroCAPI/HelloController.cs
@@ -12,4 +12,6 @@ namespace IntroCAPI
             return "Hello, from the API!";
         }
     }
+
+    // in order to call this file with a "curl" command use: "curl http://localhost:5000/api/hello"
 }
